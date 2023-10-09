10f8945399d8f6616023b16d3a3ee85421a74f62
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-10-09 17:59:01 +0300

    initialized repository

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function include(url) {
+    let script = document.createElement('script')
+    script.src = url
+    script.defer = true
+    document.getElementsByTagName('head')[0].appendChild(script)
+}
+// function getFileNameFromUrl(url) {
+//     return decodeURI(url.split('/').pop());
+// }
+function getLastElementFromUrl(url) {
+    const urlWithoutQuery = url.split('?')[0];
+
+    const segments = urlWithoutQuery.split('/');
+
+    const filteredSegments = segments.filter(segment => segment !== '');
+
+    const lastElement = filteredSegments[filteredSegments.length - 1];
+
+    return lastElement;
+}
+
