c1067ec646f2be83727d1cbcc8b59637b946c2b2
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-10-14 09:31:24 +0300

    updated subscribe info modal window

--- assets/js/modal-windows--full-screen.js
+++ assets/js/modal-windows--full-screen.js
@@ -2,10 +2,12 @@ const modalWindowsBtns = document.querySelectorAll('.btn-modal')
 
 modalWindowsBtns.forEach((e) => {
     e.onclick = () => {
+        document.body.style.overflow = "hidden"
         const currentModalWindow = document.querySelector(`#${e.id}-modal`)
         currentModalWindow.classList.toggle('hidden')
         currentModalWindow.querySelector('.close-modal').onclick = () => {
             currentModalWindow.classList.toggle('hidden')
+            document.body.style.overflow = ""
         }
     }
 })
