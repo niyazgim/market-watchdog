e3a0b671350daab0169abc152763af842bff288f
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-10-17 16:26:17 +0300

    fixed right body indent when opening modal window

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -21,6 +21,28 @@ function checkAllFields(form) {
     return true
 }
 
+function getScrollbarWidth() {
+
+    // Creating invisible container
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll'; // forcing scrollbar to appear
+    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+    document.body.appendChild(outer);
+
+    // Creating inner element and placing it in the container
+    const inner = document.createElement('div');
+    outer.appendChild(inner);
+
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
+
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer);
+
+    return scrollbarWidth;
+}
+
 function isSizeValid(min = 0, max, input) {
     inputLen = input.value.length
     if (inputLen < min) {
--- assets/js/modal-windows--full-screen.js
+++ assets/js/modal-windows--full-screen.js
@@ -3,11 +3,13 @@ const modalWindowsBtns = document.querySelectorAll('.btn-modal')
 modalWindowsBtns.forEach((e) => {
     e.onclick = () => {
         document.body.style.overflow = "hidden"
+        document.body.style.paddingRight = getScrollbarWidth() + 'px'
         const currentModalWindow = document.querySelector(`#${e.id}-modal`)
         currentModalWindow.classList.toggle('hidden')
         currentModalWindow.querySelector('.close-modal').onclick = () => {
             currentModalWindow.classList.toggle('hidden')
             document.body.style.overflow = ""
+            document.body.style.paddingRight = 0
         }
     }
 })
