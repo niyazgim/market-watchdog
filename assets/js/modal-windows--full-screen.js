ce7f192a866f7c4b4b44fcefce0913cf61307423
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-10-18 09:31:05 +0300

    made subscribe info modals

--- assets/js/modal-windows--full-screen.js
+++ assets/js/modal-windows--full-screen.js
@@ -6,6 +6,37 @@ modalWindowsBtns.forEach((e) => {
         document.body.style.paddingRight = getScrollbarWidth() + 'px'
         const currentModalWindow = document.querySelector(`#${e.id}-modal`)
         currentModalWindow.classList.toggle('hidden')
+        if(currentModalWindow.id = 'subscribe-info-modal') {
+            const planInfo = plansContent[e.dataset.planId]
+
+            currentModalWindow.querySelector('.subscribe__name').textContent = planInfo['name']
+            currentModalWindow.querySelector('.subscribe__desc').textContent = planInfo['desc']
+
+            const planBenefits = planInfo['benefits'];
+            if(planBenefits) {
+                const planBenefitsContainer = currentModalWindow.querySelector('.subscribe__benefits')
+                let planBenefitsText = ''
+                for(let i of planBenefits) {
+                    planBenefitsText += `
+                        <div class="benefit">
+                            <img src="assets/img/icons/${i['img']}" alt="infinite_icon">
+                            <span>${i['text']}</span>
+                        </div>`
+                    planBenefitsContainer.innerHTML = planBenefitsText
+                }
+            }
+
+            if(planInfo['priceOld']) {
+                priceText = `
+                    <p class="total__old-price">
+                        ${planInfo['priceOld']}
+                    </p>
+                    <p class="total__actual-price">
+                        ${planInfo['price']} <span class="price-currency-period">₽/мес.</span>
+                    </p>`
+                currentModalWindow.querySelector('.total__price_container').innerHTML = priceText
+            }
+        }
         currentModalWindow.querySelector('.close-modal').onclick = () => {
             currentModalWindow.classList.toggle('hidden')
             document.body.style.overflow = ""
---
+++ assets/js/plans-modal-content.js
@@ -0,0 +1,50 @@
+const plansContent = {
+    '1': {
+        'name': 'Базовый',
+        'desc': 'Только всё самое нужное.',
+        'benefits': [
+            {
+                'img': 'sell--corporative-color.svg',
+                'text': 'Лучшая цена за функционал'
+            }
+        ],
+        'price': '600',
+        'priceOld': '500',
+    },
+    '2': {
+        'name': 'Базовый+',
+        'desc': 'Экономьте без ограничений.',
+        'benefits': [
+            {
+                'img': 'all_inclusive--corporative-color.svg',
+                'text': 'Безлимитные переходы'
+            },
+            {
+                'img': 'heart_plus--corporative-color.svg',
+                'text': 'Можно добавлять в избранное'
+            }
+        ],
+        'price': '650',
+        'priceOld': '750',
+    },
+    '3': {
+        'name': 'Бизнес',
+        'desc': 'Для профи в своей сфере',
+        'benefits': [
+            {
+                'img': 'all_inclusive--corporative-color.svg',
+                'text': 'Безлимитные переходы'
+            },
+            {
+                'img': 'heart_plus--corporative-color.svg',
+                'text': 'Можно добавлять в избранное'
+            },
+            {
+                'img': 'north_east--corporative-color.svg',
+                'text': 'Самое лучшее для бизнеса'
+            }
+        ],
+        'price': '1500',
+        'priceOld': '1000',
+    }
+}
