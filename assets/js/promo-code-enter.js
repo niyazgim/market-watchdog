36840fe78ae1c2a7a1ed0d18453920a083d0164e
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-10-19 10:31:15 +0300

    made promo code enter

--- assets/js/promo-code-enter.js
+++ assets/js/promo-code-enter.js
@@ -4,9 +4,18 @@ promoCodeForm.onsubmit = (e) => {
     e.preventDefault()
 
     if(promoCodeForm.promoCode.value === 'TEST') {
-        promoCodeForm.querySelector('.promo-code-res').textContent = 'промокод успешно активирован'
+        promoCodeForm.querySelector('.input__status-text').textContent = 'промокод успешно активирован'
+        promoCodeForm.querySelector('.input__status-text').style.color = 'var(--text-color--valid)'
+        promoCodeForm.querySelector('.input__err-img').classList.add('hidden')
+        promoCodeForm.querySelector('.form__input').classList.remove('invalid')
+        promoCodeForm.querySelector('.form__input').classList.add('valid')
     }
     else {
-        promoCodeForm.querySelector('.promo-code-res').textContent = 'неверный промокод'
+        if(promoCodeForm.promoCode.value) promoCodeForm.querySelector('.input__status-text').textContent = 'Неверный промокод'
+        else promoCodeForm.querySelector('.input__status-text').textContent = 'Введите промокод'
+        promoCodeForm.querySelector('.input__status-text').style.color = 'var(--text-color--invalid)'
+        promoCodeForm.querySelector('.input__err-img').classList.remove('hidden')
+        promoCodeForm.querySelector('.form__input').classList.add('invalid')
+        promoCodeForm.querySelector('.form__input').classList.remove('valid')
     }
 }
