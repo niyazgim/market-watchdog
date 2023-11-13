477eb18cf840912027c348d34342f58d000c9046
Гимадиев Нияз Наилевич <252004@mck-ktits.ru>
2023-11-13 18:25:58 +0300

    updated view

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 const staticPath = path.join(__dirname, "/public")
 app.use(express.static(staticPath))
 
@@ -16,8 +18,17 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'app/views'));
 hbs.registerPartials(__dirname + '/app/views/partials');
 
-app.get('/', (req, res) => {
-    res.render('admin')
+app.use('/', require('./app/routes/root'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    // if (req.accepts('html')) {
+    //     res.sendFile(path.join(__dirname, 'views', '404.html'));
+    // } else if (req.accepts('json')) {
+    //     res.json({ "error": "404 Not Found" });
+    // } else {
+    //     res.type('txt').send("404 Not Found");
+    // }
 });
 
 app.listen(3000);
